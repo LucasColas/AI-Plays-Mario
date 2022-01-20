a777229d60e616a0bcc3fab3f1d85f54d3f93407
LucasColas <lucas.colas13@gmail.com>
2022-01-20 07:01:49 +0100

    update

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
-print(SIMPLE_MOVEMENT) #7 different types of actions
+#print(SIMPLE_MOVEMENT) #7 different types of actions
 
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+env = JoypadSpace(env, SIMPLE_MOVEMENT) #Wrapping environment to use simple actions
+#print(env.action_space)
+env.observation_space.shape
