a2cc2681d1ca16a333f0395abd2ccc71cc0fbd94
LucasColas <lucas.colas13@gmail.com>
2022-01-21 08:20:33 +0100

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
 
-from gym.wrappers import FrameStack, GrayScaleObservation #Stack Frames (our AI will be able
+from gym.wrappers import GrayScaleObservation #Stack Frames (our AI will be able
 #to see. GrayScaleObservation to convert images in gray = less data to process)
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
 from matplotlib import pyplot as plt
@@ -16,8 +16,8 @@ env = gym_super_mario_bros.make('SuperMarioBros-v0')
 #print(env.action_space)
 print(env.observation_space.shape) #Observation we get
 
-plt.imshow(env.reset())
-plt.show()
+#plt.imshow(env.reset())
+#plt.show()
 #Running the game
 """
 done = True
@@ -39,7 +39,22 @@ Preprocessing
 env = JoypadSpace(env, SIMPLE_MOVEMENT) #Wrapping environment to use simple actions
 
 #Grayscaling
-env = GrayScaleObservation(env, keep_dim=True) #Keep dim to be able to do stackframe
+env = GrayScaleObservation(env, keep_dim=True) #Keep dim to be able to do stack the frames
 print("after grayscaling",env.reset().shape)
-plt.imshow(env.reset())
+#plt.imshow(env.reset())
+#plt.show()
+
+#Wrap inside the dummy environment
+env = DummyVecEnv([lambda: env]) #we return env in a list
+print("new shape : ", env.reset().shape)
+
+#Stack the frames
+env = VecFrameStack(env, 4, channels_order='last') #We choose to stack 4 frames.
+print(env.reset().shape) #(1,x,y,4) because we stacked 4 frames
+
+state, reward, done, info = env.step([env.action_space.sample()]) #in a list because we vectorized our environment
+plt.figure(figsize=(6,4))
+for i in range(state.shape[3]):
+    plt.subplot(1,4,i+1)
+    plt.imshow(state[0][:,:,i])
 plt.show()
